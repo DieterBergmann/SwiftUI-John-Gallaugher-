b1e5ea29730fd511335e1303e606d9c9fd8ab3a8
Dieter Bergmann <dieterbergmann@arcor.de>
2023-12-29 22:08:57 +0100

    Ch. 1.6 Getting SwiftUI Layout Right Part 2: Spacers, Colors, safeArea & More! (You Are Awesome app)

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,31 +11,52 @@ struct ContentView: View {
     @State private var messageString = "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
     
     var body: some View {
-        VStack {
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(Color.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.orange, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                .padding()
+        ZStack {
+            Rectangle()
+                .fill(.yellow.gradient)
+                .ignoresSafeArea()
             
-            HStack {
-                Button("Awesome") {
-                    // This is the action
-                    messageString = "Your Are Awsome!"
-                }
-                .buttonStyle(.borderedProminent)
+            VStack {
+                Text("You Have Skills!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color("Gold-BC"))
+                    .padding()
+                    .background(Color("Maroon-BC"))
+                    .cornerRadius(15)
                 
-                Button("Great") {
-                    messageString = "Great!"
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(Color.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+    //                .border(.orange, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .padding()
+                
+                Spacer()
+                
+                HStack {
+                    Button("Awesome") {
+                        // This is the action
+                        messageString = "Your Are Awsome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
                 }
-                .buttonStyle(.borderedProminent)
+    //            .border(.purple, width: 5)
+                .padding()
             }
-            .border(.purple, width: 5)
         }
     }
 }
