feeab4d297818f150003ec3af973b9122fbcf82a
Dieter Bergmann <dieterbergmann@arcor.de>
2023-12-30 15:26:22 +0100

    Ch. 1.8 SwiftUI System Images + SF Symbols (You Are Awesome app)

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,52 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    @State private var messageString = "Namaste"
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                    Spacer()
-                    
-                    Text(messageString)
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .minimumScaleFactor(0.5)
-                        .multilineTextAlignment(.center)
-                        .foregroundStyle(Color.red)
-                        .frame(height: 150)
-                        .frame(maxWidth: .infinity)
-        //                .border(.orange, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                        .padding()
-                    
-                    Spacer()
-                    
-                    Divider()
-                        .background(.black)
-                        .padding()
-                        .frame(width: 150.0)
-                    
-                    Rectangle()
-                        .fill(.indigo)
-                        .frame(width: geometry.size.width * (2/3), height: 1)
-                    
-                    HStack {
-                        Button("Awesome") {
-                            // This is the action
-                            messageString = "Your Are Awsome!"
-                        }
-                        .buttonStyle(.borderedProminent)
-                        
-                        Spacer()
-                        
-                        Button("Great") {
-                            messageString = "Great!"
-                        }
-                        .buttonStyle(.borderedProminent)
-                    }
-        //            .border(.purple, width: 5)
-                    .padding()
+        VStack {
+            Spacer()
+            
+            Image(systemName: "cloud.sun.rain.fill")
+                .resizable()
+                .scaledToFit()
+                .symbolRenderingMode(.multicolor)
+                .padding()
+                .background(Color(hue: 0.52, saturation: 0.299, brightness: 0.958))
+                .cornerRadius(30)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(.teal, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                )
+                .padding()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    // This is the action
+                    messageString = "Your Are Awsome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                Button("Great") {
+                    messageString = "Great!"
+                }
+                .buttonStyle(.borderedProminent)
             }
+            .padding()
         }
     }
 }
