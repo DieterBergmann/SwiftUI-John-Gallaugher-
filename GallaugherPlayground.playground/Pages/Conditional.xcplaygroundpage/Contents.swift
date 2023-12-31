b03e8fa000f94ebb4945ede479b118840bf1e90c
Dieter Bergmann <dieterbergmann@arcor.de>
2023-12-31 08:48:49 +0100

    Ch. 1.9 Swift Conditionals, Constants, Print, and Scope (You Are Awesome SwiftUI app)

---
+++ GallaugherPlayground.playground/Pages/Conditional.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var messageString = "I am a Developer!"
+//print("Hello There!" +  messageString)
+//print("Hello There! \(messageString)") // String Interpolation
+
+var person = "Limor"
+
+if person == "Limor" {
+    print("Hello, Lady Ada !")
+}
+
+
--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,24 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "Namaste"
+    @State private var messageString = ""
     
     var body: some View {
         VStack {
             Spacer()
-            
-            Image(systemName: "cloud.sun.rain.fill")
-                .resizable()
-                .scaledToFit()
-                .symbolRenderingMode(.multicolor)
-                .padding()
-                .background(Color(hue: 0.52, saturation: 0.299, brightness: 0.958))
-                .cornerRadius(30)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 30)
-                        .stroke(.teal, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-                )
-                .padding()
             
             Text(messageString)
                 .font(.largeTitle)
@@ -39,22 +26,20 @@ struct ContentView: View {
             
             Spacer()
             
-            HStack {
-                Button("Awesome") {
-                    // This is the action
-                    messageString = "Your Are Awsome!"
-                }
-                .buttonStyle(.borderedProminent)
+            Button("Show Message") {
+                let message1 = "You are Awesome !"
+                let message2 = "You Are Great !"
                 
-                Spacer()
-                
-                Button("Great") {
-                    messageString = "Great!"
+                // This is the action
+                if messageString == message1 {
+                   messageString = message2
+                } else {
+                    messageString = message1
                 }
-                .buttonStyle(.borderedProminent)
             }
-            .padding()
+            .buttonStyle(.borderedProminent)
         }
+        .padding()
     }
 }
 
