e1e1cd4fc51ab5a1da1e0143541b5a710c00e03f
Dieter Bergmann <dieterbergmann@arcor.de>
2024-01-06 20:19:08 +0100

    Ch 1.21 SwiftUI Two Way Bindings for @State using $ (You Are Awesome app)

---
+++ TwoWayBindings/TwoWayBindings/TwoWayBindings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwoWayBindings
+//
+//  Created by Dieter Bergmann on 06.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
